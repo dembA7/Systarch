fd757add8b3bd411a0963de43777654b830d40d9
Alan <A01708668@tec.com>
2023-03-29 22:14:19 -0600

    Subir script de base de datos corregida

--- db/script.sql
+++ db/script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 09, 2023 at 09:29 PM
+-- Generation Time: Mar 30, 2023 at 04:11 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -29,9 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `epics` (
   `epic_ID` varchar(100) NOT NULL,
-  `user_ID` varchar(100) DEFAULT NULL,
-  `ticket_ID` varchar(100) DEFAULT NULL,
-  `project_ID` varchar(100) DEFAULT NULL
+  `epic_Summ` varchar(100) NOT NULL,
+  `project_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -41,9 +40,8 @@ CREATE TABLE `epics` (
 --
 
 CREATE TABLE `projects` (
-  `project_ID` varchar(100) NOT NULL,
-  `project_Name` varchar(100) NOT NULL,
-  `report_ID` varchar(100) DEFAULT NULL
+  `project_ID` int(11) NOT NULL,
+  `project_Name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -55,7 +53,8 @@ CREATE TABLE `projects` (
 CREATE TABLE `reports` (
   `report_ID` varchar(100) NOT NULL,
   `report_Progress` int(100) DEFAULT NULL,
-  `report_Estimated` int(100) DEFAULT NULL
+  `report_Estimated` int(100) DEFAULT NULL,
+  `project_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,13 +64,16 @@ CREATE TABLE `reports` (
 --
 
 CREATE TABLE `tickets` (
-  `ticket_ID` varchar(100) NOT NULL,
+  `ticket_ID` int(11) NOT NULL,
   `ticket_Key` varchar(100) NOT NULL,
+  `ticket_Summary` varchar(100) NOT NULL,
   `ticket_Status` varchar(50) DEFAULT NULL,
-  `ticket_Points` int(15) DEFAULT NULL,
+  `ticket_Points` int(11) DEFAULT NULL,
   `ticket_Type` char(5) DEFAULT NULL,
   `ticket_Label` varchar(300) DEFAULT NULL,
-  `ticket_Update` date DEFAULT NULL
+  `epic_ID` varchar(100) NOT NULL,
+  `user_ID` varchar(80) NOT NULL,
+  `ticket_Update` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -81,7 +83,7 @@ CREATE TABLE `tickets` (
 --
 
 CREATE TABLE `users` (
-  `user_ID` varchar(100) NOT NULL,
+  `user_ID` varchar(80) NOT NULL,
   `user_Password` varchar(100) NOT NULL,
   `user_Name` varchar(100) NOT NULL,
   `user_Phone` int(10) DEFAULT NULL,
@@ -99,34 +101,56 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `epics`
   ADD PRIMARY KEY (`epic_ID`),
-  ADD KEY `user_ID` (`user_ID`,`ticket_ID`,`project_ID`),
-  ADD KEY `project_ID` (`project_ID`),
-  ADD KEY `ticket_ID` (`ticket_ID`);
+  ADD KEY `project_ID` (`project_ID`);
 
 --
 -- Indexes for table `projects`
 --
 ALTER TABLE `projects`
-  ADD PRIMARY KEY (`project_ID`),
-  ADD KEY `report_ID` (`report_ID`);
+  ADD PRIMARY KEY (`project_ID`);
 
 --
 -- Indexes for table `reports`
 --
 ALTER TABLE `reports`
-  ADD PRIMARY KEY (`report_ID`);
+  ADD PRIMARY KEY (`report_ID`),
+  ADD KEY `project_ID` (`project_ID`);
 
 --
 -- Indexes for table `tickets`
 --
 ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`ticket_ID`);
+  ADD PRIMARY KEY (`ticket_ID`),
+  ADD KEY `epic_ID` (`epic_ID`),
+  ADD KEY `user_ID` (`user_ID`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `epics`
+--
+ALTER TABLE `epics`
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -136,15 +160,20 @@ ALTER TABLE `users`
 -- Constraints for table `epics`
 --
 ALTER TABLE `epics`
-  ADD CONSTRAINT `epics_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`),
-  ADD CONSTRAINT `epics_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`),
-  ADD CONSTRAINT `epics_ibfk_3` FOREIGN KEY (`ticket_ID`) REFERENCES `tickets` (`ticket_ID`);
+  ADD CONSTRAINT `epics_ibfk_1` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`);
 
 --
--- Constraints for table `projects`
+-- Constraints for table `reports`
 --
-ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`report_ID`) REFERENCES `reports` (`report_ID`);
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`epic_ID`) REFERENCES `epics` (`epic_ID`),
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
