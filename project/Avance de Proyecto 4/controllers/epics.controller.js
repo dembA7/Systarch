8a7c524263e7469e3a1dec3141142d2c4cd6f75d
Ian326 <a01708940@tec.mx>
2023-03-30 23:28:40 -0600

    Updated 'session.message' from 'uploadCSV' view.

--- project/Avance de Proyecto 4/controllers/epics.controller.js
+++ project/Avance de Proyecto 4/controllers/epics.controller.js
@@ -4,16 +4,18 @@ const multer = require('multer');
 const pruebas = express();
 
 exports.get_import = (request, response, next) => {
+  const msg = request.session.mensaje
+  request.session.mensaje = ''
   response.render('uploadCSV', {
     isLoggedIn: request.session.isLoggedIn || false,
     nombre: request.session.nombre || '',
-    mensaje: request.session.mensaje || '',
+    mensaje: msg || '',
   });
 };
 
 exports.post_import = (request, response, next) => {
   if(request.session.mensaje == 'Formato de archivo no válido, por favor, intenta de nuevo.'){
-    response.redirect('/epics/import')
+    response.redirect('/epics/import');
   }
   else{
     console.log(request.file)
