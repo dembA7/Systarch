017f6b08477925a58208cf93892e59d1b2fef51f
Ian326 <a01708940@tec.mx>
2023-03-30 20:46:03 -0600

    Completed 'Importar CSV' functionality
    Co-authored-by: Riosmena <Riosmena@users.noreply.github.com>

--- project/Avance de Proyecto 4/app.js
+++ project/Avance de Proyecto 4/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const session = require('express-session');
 const csrf = require('csurf');
 const isAuth = require('./util/is-auth');
+const multer = require('multer');
 
 const app = express();
 
@@ -16,6 +17,35 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+//FileSystem
+
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'public/epics/upload');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null, new Date().getMilliseconds() + '-' + file.originalname);
+    },
+});
+
+const fileFilter = (request, file, callback) => {
+    if (file.mimetype == 'text/csv') {
+      request.session.mensaje = '';
+      console.log("CSV added to public folder successfully.");
+      callback(null, true);
+    }
+    else {
+        request.session.mensaje = 'Error';
+        console.log("Nothing added to public folder. Error: Invalid type file");
+        callback(null, false);
+    }
+}
+
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('csvUploaded'));
 
 // Views
 app.set('view engine', 'ejs');
--- project/Avance de Proyecto 4/controllers/epics.controller.js
+++ project/Avance de Proyecto 4/controllers/epics.controller.js
@@ -3,34 +3,6 @@ const multer = require('multer');
 
 const pruebas = express();
 
-//FileSystem
-
-const fileStorage = multer.diskStorage({
-    destination: (request, file, callback) => {
-        //'uploads': Es el directorio del servidor donde se subirán los archivos 
-        callback(null, 'public/epics/upload');
-    },
-    filename: (request, file, callback) => {
-        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
-        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
-        callback(null, new Date().getMilliseconds() + '-' + file.originalname);
-    },
-});
-
-const fileFilter = (request, file, callback) => {
-    if (file.mimetype == 'text/csv') {
-      console.log("CSV added to public folder successfully.");
-      callback(null, true);
-    }
-    else {
-        request.session.mensaje = 'Error';
-        console.log("Invalid type file");
-        callback(null, false);
-    }
-}
-
-pruebas.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('csvUploaded'));
-
 exports.get_import = (request, response, next) => {
   response.render('uploadCSV', {
     isLoggedIn: request.session.isLoggedIn || false,
@@ -41,7 +13,7 @@ exports.get_import = (request, response, next) => {
 
 exports.post_import = (request, response, next) => {
   if(request.session.mensaje == 'Error'){
-    response.redirect('/import')
+    response.redirect('/epics/import')
   }
   else{
     console.log(request.file)
