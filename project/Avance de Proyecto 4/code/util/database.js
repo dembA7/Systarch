2dfcb5ac3078f50e108edab147356e24e63c2973
Alan Patricio González Bernal <alanpagzz@MacBook-Pro-de-Alan-2.local>
2023-03-31 13:00:51 -0600

    Se prepara para subir la visualización de avance de proyecto·

--- project/Avance de Proyecto 4/code/util/database.js
+++ project/Avance de Proyecto 4/code/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'systarch',
-    password: '',
+    password: 'root',
 });
 
 module.exports = pool.promise();
