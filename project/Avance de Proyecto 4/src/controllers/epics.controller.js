0fe468380bbdff28409232a331b2912259807d37
Alan Patricio González Bernal <alanpagzz@MacBook-Pro-de-Alan-2.local>
2023-04-04 16:33:11 -0600

    Se trabaja en la inserción de la página de visualización (incompleto)

--- project/Avance de Proyecto 4/src/controllers/epics.controller.js
+++ project/Avance de Proyecto 4/src/controllers/epics.controller.js
@@ -82,3 +82,14 @@ async function nextPage(flpath){
   console.log("Esto va segundo")
   console.log(datos[1]);
 };
+
+exports.get_detail = (request, response, next) => {
+  const msg = request.session.mensaje
+  request.session.mensaje = ''
+  console.log("Se solicitan detalles del epic")
+  response.render('epic detail', {
+    isLoggedIn: request.session.isLoggedIn || false,
+    nombre: request.session.nombre || '',
+    mensaje: msg || '',
+  });
+};
--- project/Avance de Proyecto 4/src/routes/epic.routes.js
+++ project/Avance de Proyecto 4/src/routes/epic.routes.js
@@ -8,6 +8,8 @@ router.get('/import', epicsController.get_import);
 
 router.post('/import', epicsController.post_import);
 
+router.post('/details', epicsController.get_detail);
+
 //router.get('/view', epicsController.get_view);
 
 //router.post('/view', epicsController.post_view);
