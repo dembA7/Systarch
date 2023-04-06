4bda83587fa45e9a5e91d9f5309b2db471089603
Emi Riosmena <a01704245@tec.mx>
2023-04-05 19:03:50 -0600

    Updated database

--- db/systarch.sql
+++ db/systarch.sql
@@ -45,7 +45,8 @@ CREATE TABLE `epics` (
 CREATE TABLE `projects` (
   `project_ID` int(100) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `project_Name` varchar(200) NOT NULL,
-  `report_ID` int(100) DEFAULT NULL
+  `report_ID` int(100) DEFAULT NULL,
+  `epic_Link` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -74,12 +75,13 @@ CREATE TABLE `tickets` (
   `Issue_Type` char(10) NOT NULL,
   `Story_Points` int(50) DEFAULT NULL,
   `ticket_Status` varchar(50) NOT NULL,
-  `ticket_Update` date NOT NULL,
+  `epic_Link` varchar(30) NOT NULL,
+  `epic_Link_Summary` varchar(400) NOT NULL,
+  `ticket_Update` timestamp NOT NULL,
   `ticket_Assignee` varchar(100) DEFAULT NULL,
   `ticket_Assignee_ID` varchar(200) DEFAULT NULL,
   `ticket_Label` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
 -- --------------------------------------------------------
 
 --
@@ -93,7 +95,9 @@ CREATE TABLE `users` (
   `user_Phone` int(10) DEFAULT NULL,
   `user_Mail` varchar(100) NOT NULL,
   `user_WeeklyAgilePoints` int(50) DEFAULT NULL,
-  `user_Skill` char(2) DEFAULT NULL
+  `user_Skill` char(2) DEFAULT NULL,
+  `ticket_Assignee` varchar(100) DEFAULT NULL,
+  `ticket_Assignee_ID` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -119,7 +123,22 @@ ALTER TABLE `epics`
 -- Indices de la tabla `projects`
 --
 ALTER TABLE `projects`
-  ADD KEY `report_ID` (`report_ID`);
+  ADD KEY `report_ID` (`report_ID`),
+  ADD KEY `epic_Link` (`epic_Link`);
+
+--
+-- Indices de la tabla `tickets`
+--
+ALTER TABLE `tickets`
+  ADD KEY `epic_Link` (`epic_Link`),
+  ADD KEY `ticket_Assignee_ID` (`ticket_Assignee_ID`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD KEY `ticket_Assignee` (`ticket_Assignee`),
+  ADD KEY `ticket_Assignee_ID` (`ticket_Assignee_ID`);
 
 --
 
@@ -132,13 +151,29 @@ ALTER TABLE `projects`
 ALTER TABLE `epics`
   ADD CONSTRAINT `epics_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`),
   ADD CONSTRAINT `epics_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`),
-  ADD CONSTRAINT `epics_ibfk_3` FOREIGN KEY (`ticket_Id`) REFERENCES `tickets` (`ticket_Id`);
+  ADD CONSTRAINT `epics_ibfk_3` FOREIGN KEY (`ticket_Id`) REFERENCES `tickets` (`ticket_Id`),
+  ADD CONSTRAINT `epics_ibfk_4` FOREIGN KEY (`epic_Link`) REFERENCES `tickets` (`epic_Link`);
+
+--
+-- Filtros para la tabla `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`ticket_Assignee`) REFERENCES `users` (`ticket_Assignee`),
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`ticket_Assignee_ID`) REFERENCES `users` (`ticket_Assignee`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`ticket_Assignee`) REFERENCES `tickets` (`ticket_Assignee`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`ticket_Assignee_ID`) REFERENCES `tickets` (`ticket_Assignee`);
 
 --
 -- Filtros para la tabla `projects`
 --
 ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`report_ID`) REFERENCES `reports` (`report_ID`);
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`report_ID`) REFERENCES `reports` (`report_ID`),
+  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`epic_Link`) REFERENCES `tickets` (`epic_Link`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
