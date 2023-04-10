50975aca1f25c6184ee4ea030612cdfdeadfdfa3
Emi Riosmena <a01704245@tec.mx>
2023-04-10 09:48:04 -0600

    Fixing branch issue

--- project/Avance de Proyecto 4/controllers/dispatch.controller.js
+++
@@ -1,6 +0,0 @@
-const Dispatch = require('../models/dispatch.model');
-const bcrypt = require('bcryptjs');
-
-exports.get_inicio = (request, response, next) => {
-    response.render('inicio')
-};
--- project/Avance de Proyecto 4/util/is-auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response, next) => {
-    if (!request.session.isLoggedIn) {
-        return response.redirect('/usuarios/login');
-    }
-    next();
-}
