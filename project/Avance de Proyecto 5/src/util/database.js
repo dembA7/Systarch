3a0b30cdf618ddb309d7199ae42fc302b7391c63
XxGonanxX <patomorelia@hotmail.com>
2023-04-13 10:28:46 -0600

    Se añade la grafica a visualización de proyectos

--- project/Avance de Proyecto 5/src/util/database.js
+++ project/Avance de Proyecto 5/src/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'systarch',
-    password: 'root',
+    password: '',
 });
 
 module.exports = pool.promise();
