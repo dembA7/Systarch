14dbc5f4ec04ab613dff0c3cbd92cc56750626ab
Alan <A01708668@tec.com>
2023-04-13 20:24:21 -0600

    Implementar barras de progreso por epic

--- project/Avance de Proyecto 5/src/controllers/dispatch.controller.js
+++ project/Avance de Proyecto 5/src/controllers/dispatch.controller.js
@@ -2,23 +2,14 @@ const Epic = require('../models/epics.model');
 
 exports.get_inicio = (request, response, next) => {
   
-  // let res = Epic.ticketsTotal('PART-289');
-  Epic.fetchAll()
-  .then(([rows_Epic, fieldData]) => {
-    Epic.ticketsTotal('PART-289')
-    .then(([total, fieldData]) => {
-      Epic.ticketsDone('PART-289')
-      .then(([done, fieldData]) => {
-        response.render('inicio', {
-          isLoggedIn: request .session.isLoggedIn || false,
-          epics: rows_Epic,
-          username: request.session.nombre,
-          titulo: "DispatchHealth",
-          ttotal: total,
-          tdone: done,
-        });
-      }).catch(err => console.log(err)); 
-    }).catch(err => console.log(err)); 
+  Epic.Progreso()
+  .then(([rows, fieldData]) => {
+    response.render('inicio', {
+      isLoggedIn: request .session.isLoggedIn || false,
+      epics: rows,
+      username: request.session.nombre,
+      titulo: "DispatchHealth",
+    });
   }).catch(err => console.log(err));
   
 
