40c0729f97a14fe9ff82da72b6739aba81eddf51
Emi Riosmena <a01704245@tec.mx>
2023-04-18 09:38:56 -0600

    Fixing branch issue

--- project/Avance de Proyecto 4/src/controllers/dispatch.controller.js
+++
@@ -1,7 +0,0 @@
-exports.get_inicio = (request, response, next) => {
-    response.render('inicio', {
-    isLoggedIn: request .session.isLoggedIn || false,
-    username: request.session.username || "",
-    titulo: "DispatchHealth",
-  });
-};
--- project/Avance de Proyecto 5/src/controllers/tickets.controller.js
+++
@@ -1,8 +0,0 @@
-const Ticket = require('../models/tickets.model');
-
-exports.get_ticket = (request, response, next) => {
-    response.render('tickets', {
-        isLoggedIn: request.session.isLoggedIn || false,
-        nombre: request.session.nombre || '',
-      });
-}
