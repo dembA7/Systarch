758cfa1418a69f4550bd0f9a8f080d83dbf11209
Ian326 <a01708940@tec.mx>
2023-04-20 13:12:50 -0600

    WIP Create Projects functionality

--- project/Avance de Proyecto 5/src/controllers/projects.controller.js
+++ project/Avance de Proyecto 5/src/controllers/projects.controller.js
@@ -1,3 +1,4 @@
+const Epic = require('../models/epics.model');
 exports.get_projects = (request, response, next) => {
     response.render('projects', {
     isLoggedIn: request .session.isLoggedIn || false,
@@ -7,9 +8,58 @@ exports.get_projects = (request, response, next) => {
 };
 
 exports.get_createProjects = (request, response, next) => {
-  response.render('create', {
-  isLoggedIn: request .session.isLoggedIn || false,
-  username: request.session.username || "",
-  titulo: "DispatchHealth",
-});
+  Epic.fetchAll()
+  .then((epics) => {
+
+    if (!request.session.epicsSelected){
+
+      request.session.epicsSelected = [];
+
+    }
+
+    else{
+      
+      for (object in epics[0]){
+
+        const epic_Link = epics[0][object].epic_Link.toString();
+        const epic_Link_Summary = epics[0][object].epic_Link_Summary.toString();
+        const index = `${epic_Link}:${epic_Link_Summary}`
+
+        if(request.session.epicsSelected.includes(index)){
+
+          delete epics[0][object]
+
+        }
+      }
+    }
+
+    response.render('create', {
+      isLoggedIn: request.session.isLoggedIn || false,
+      username: request.session.username || "",
+      titulo: "DispatchHealth",
+      epics: epics[0] || [],
+      epicsSelected: request.session.epicsSelected
+      
+    });
+  })
+};
+
+exports.postAdd_createProjects = (request, response, next) => {
+  console.log("Post de Add");
+  request.session.epicsSelected.push(request.body.selEpic);
+  response.redirect("/projects/create");
+};
+
+exports.postRemove_createProjects = (request, response, next) => {
+  console.log("Post de Remove");
+  request.body.delEpic;
+  console.log(request.body.delEpic);
+  
+  for (i in request.session.epicsSelected){
+    console.log()
+    if (request.session.epicsSelected.includes(request.body.delEpic)){
+      delete request.session.epicsSelected[i]
+    }
+  }
+  response.redirect("/projects/create")
 };
--- project/Avance de Proyecto 5/src/routes/projects.routes.js
+++ project/Avance de Proyecto 5/src/routes/projects.routes.js
@@ -7,5 +7,7 @@ const projectsController = require('../controllers/projects.controller');
 router.get('/', projectsController.get_projects);
 
 router.get('/create', projectsController.get_createProjects);
+router.post('/create', projectsController.postAdd_createProjects);
+router.post('/create/r', projectsController.postRemove_createProjects);
 
-module.exports = router;
+module.exports = router;    
