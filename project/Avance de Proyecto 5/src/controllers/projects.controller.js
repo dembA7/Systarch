fccb5fb5dcc12b3c8830639d7c7b04d3b2daa58b
Ian326 <a01708940@tec.mx>
2023-04-20 15:52:50 -0600

    WIP Create a Proyect fnunctionality

--- project/Avance de Proyecto 5/src/controllers/projects.controller.js
+++ project/Avance de Proyecto 5/src/controllers/projects.controller.js
@@ -12,27 +12,30 @@ exports.get_createProjects = (request, response, next) => {
   .then((epics) => {
 
     if (!request.session.epicsSelected){
-
+      console.log("epicsSelected vacios")
       request.session.epicsSelected = [];
 
     }
 
-    else{
-      
-      for (object in epics[0]){
+    else if(request.session.epicsSelected.length > 0){
 
-        const epic_Link = epics[0][object].epic_Link.toString();
-        const epic_Link_Summary = epics[0][object].epic_Link_Summary.toString();
-        const index = `${epic_Link}:${epic_Link_Summary}`
+      console.log(request.session.epicsSelected)
 
-        if(request.session.epicsSelected.includes(index)){
+      for (object1 in epics[0]){
 
-          delete epics[0][object]
+        for (object2 in request.session.epicsSelected){
 
+          if(request.session.epicsSelected[object2] != null
+          && request.session.epicsSelected[object2].epic_ID == epics[0][object1].epic_ID){
+
+              delete epics[0][object1];
+              break;
+              
+          }
         }
       }
     }
-
+    
     response.render('create', {
       isLoggedIn: request.session.isLoggedIn || false,
       username: request.session.username || "",
@@ -44,21 +47,43 @@ exports.get_createProjects = (request, response, next) => {
   })
 };
 
-exports.postAdd_createProjects = (request, response, next) => {
+exports.postAdd_createProjects = async(request, response, next) => {
+
   console.log("Post de Add");
-  request.session.epicsSelected.push(request.body.selEpic);
+  const addProj = request.body.selEpic.split(":");
+  console.log(addProj)
+  let add;
+  epics = await Epic.fetchAll();
+
+  for(object in epics[0]){
+
+    if(addProj[0] == epics[0][object].epic_Link
+      && addProj[1] == epics[0][object].epic_Link_Summary){
+
+      console.log("Añadiendo epic a lista")
+      add = epics[0][object];
+    }
+  }
+
+  request.session.epicsSelected.push(add)
   response.redirect("/projects/create");
 };
 
 exports.postRemove_createProjects = (request, response, next) => {
   console.log("Post de Remove");
-  request.body.delEpic;
-  console.log(request.body.delEpic);
   
-  for (i in request.session.epicsSelected){
-    console.log()
-    if (request.session.epicsSelected.includes(request.body.delEpic)){
-      delete request.session.epicsSelected[i]
+  if (request.body.delEpic){
+    const delProj = request.body.delEpic.split(":");
+    for (i in request.session.epicsSelected){
+
+      if(request.session.epicsSelected[i] != null
+        && request.session.epicsSelected[i].epic_Link == delProj[0]
+        && request.session.epicsSelected[i].epic_Link_Summary == delProj[1]){
+
+          delete request.session.epicsSelected[i]
+          break;
+
+      }
     }
   }
   response.redirect("/projects/create")
