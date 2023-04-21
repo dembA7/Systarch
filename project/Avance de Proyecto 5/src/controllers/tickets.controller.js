e73add0b86f900b30fdc06535e972250082ab9d5
unknown <a01709522@tec.mx>
2023-04-21 02:59:22 -0600

    update front-end

--- project/Avance de Proyecto 5/src/controllers/tickets.controller.js
+++ project/Avance de Proyecto 5/src/controllers/tickets.controller.js
@@ -4,7 +4,7 @@ const Ticket = require('../models/tickets.model');
 exports.get_ticket = (request, response, next) => {
   return Ticket.viewTicket()
   .then(([tickets]) => {
-    console.log("Requested tickets details")
+    // console.log("Requested tickets details")
     response.render('tickets', {
       isLoggedIn: request.session.isLoggedIn || false,
       nombre: request.session.nombre || '',
