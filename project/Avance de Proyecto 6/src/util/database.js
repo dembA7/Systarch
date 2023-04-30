93a642377aa1df6f36594b677963c7489b36c0ed
unknown <a01709522@tec.mx>
2023-04-30 17:04:39 -0600

    change db user

--- project/Avance de Proyecto 6/src/util/database.js
+++ project/Avance de Proyecto 6/src/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'demba',
-    database: 'systarch',
+    database: 'Systarch',
     password: '12345',
 });
 
