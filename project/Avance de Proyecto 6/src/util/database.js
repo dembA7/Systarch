056910d7202355a5c4c23e13376f4cc734fa2962
Alan Patricio González <patomorelia@hotmail.com>
2023-05-01 21:42:29 -0600

    Se arregla database.js

--- project/Avance de Proyecto 6/src/util/database.js
+++ project/Avance de Proyecto 6/src/util/database.js
@@ -3,16 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     
     host: 'localhost',
-    user: 'root',
-    database: 'systarch',
-    password: '',
-
-    /*
-    host: 'localhost',
     user: 'demba',
     database: 'Systarch',
     password: '12345',
-    */
 
 });
 
