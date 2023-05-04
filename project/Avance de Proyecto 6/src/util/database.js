884ae2cecbf61055fb959290a4bbfd334dbdc7e5
XxGonanxX <patomorelia@hotmail.com>
2023-05-04 17:37:40 -0600

    Formato para projectDetail

--- project/Avance de Proyecto 6/src/util/database.js
+++ project/Avance de Proyecto 6/src/util/database.js
@@ -1,18 +1,19 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    
+
+    /*
     host: 'localhost',
     user: 'root',
     database: 'systarch',
     password: '',
+    */
 
-/*
     host: 'localhost',
     user: 'demba',
     database: 'Systarch',
     password: '12345',
-*/
+
 
 });
 
