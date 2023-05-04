96afa438b43877ea798e03b888ff93aed89d35d5
unknown <a01709522@tec.mx>
2023-05-03 23:59:55 -0600

    update database.js

--- project/Avance de Proyecto 5/src/util/database.js
+++ project/Avance de Proyecto 5/src/util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    database: 'systarch',
-    password: '',
+    user: 'demba',
+    database: 'Systarch',
+    password: '12345',
 });
 
 module.exports = pool.promise();
