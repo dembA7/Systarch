ea683cb50415ddbee93fcdcb5bf89f545b0c9b1e
unknown <a01709522@tec.mx>
2023-05-04 21:23:29 -0600

    debugging

--- project/Avance de Proyecto 6/src/models/project.model.js
+++ project/Avance de Proyecto 6/src/models/project.model.js
@@ -52,17 +52,17 @@ module.exports = class Project {
     }
 
     static progress(ProjectProgress){
-    return db.execute(`
-        SELECT Project_Name, ROUND(AVG(progreso),1) AS progreso
-        FROM (
-        SELECT p.project_Name, get_progreso(e.epic_Link) AS progreso
-        FROM project_epics pe, projects p, epics e
-        WHERE p.project_ID = pe.project_ID
-        AND e.epic_ID = pe.epic_ID
-        AND p.project_Name = ?
-        ) AS progress;
-            `,[ProjectProgress])
-            ;
+        return db.execute(`
+            SELECT project_Name, ROUND(AVG(progreso),1) AS progreso
+            FROM (
+            SELECT p.project_Name, get_progreso(e.epic_Link) AS progreso
+            FROM project_epics pe, projects p, epics e
+            WHERE p.project_ID = pe.project_ID
+            AND e.epic_ID = pe.epic_ID
+            AND p.project_Name = ?
+            ) AS progress;
+                `,[ProjectProgress])
+                ;
     }
 
     static fetchTickets(id){
