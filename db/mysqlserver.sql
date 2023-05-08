fe5388a7e6cbd869b065c37c78b38ca54e25cdf2
dembo <a01709522@tec.mx>
2023-05-07 18:04:57 -0600

    update db scripts

--- db/mysqlserver.sql
+++ db/mysqlserver.sql
@@ -2,22 +2,14 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2023 a las 09:48:05
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
-
--------------------------------------------------
--- Archivo para creacion de db desde 0 en MySQL Workbench o en la terminal.
--------------------------------------------------
-
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2023 at 01:38 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-CREATE DATABASE Systarch;
-USE Systarch;
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,19 +18,19 @@ USE Systarch;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `systarch`
+-- Database: `systarch`
 --
 
 DELIMITER $$
 --
--- Procedimientos
+-- Procedures
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spProgreso` (IN `_epic_Link` VARCHAR(30), OUT `progreso` INT)   BEGIN
   DECLARE total_tickets INT DEFAULT 0;
   DECLARE total_status INT DEFAULT 0;
   
   SELECT COUNT(*) INTO total_tickets FROM tickets WHERE epic_Link = _epic_Link;
-  SELECT COUNT(*) INTO total_status FROM tickets WHERE epic_Link = _epic_Link AND ticket_Status IN ('Done', 'Closed', 'Canceled');
+  SELECT COUNT(*) INTO total_status FROM tickets WHERE epic_Link = _epic_Link AND ticket_Status IN ('Done', 'Closed','Canceled');
   
   IF total_tickets > 0 THEN
     SET progreso = (total_status / total_tickets) * 100;
@@ -48,17 +40,15 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `spProgreso` (IN `_epic_Link` VARCHA
 END$$
 
 --
--- Funciones
+-- Functions
 --
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_progreso` (`epic_Link` VARCHAR(30)) RETURNS INT(11) DETERMINISTIC
-BEGIN
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_progreso` (`epic_Link` VARCHAR(30)) RETURNS INT(11)  BEGIN
     DECLARE progreso INT;
     CALL spProgreso(epic_Link, @progreso);
     RETURN @progreso;
 END$$
 
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_Sprints` (`_epic_Link` VARCHAR(30)) RETURNS FLOAT DETERMINISTIC
-BEGIN
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_Sprints` (`_epic_Link` VARCHAR(30)) RETURNS FLOAT  BEGIN
   DECLARE total_points FLOAT(11,2) DEFAULT 0;
   DECLARE total_teamAP FLOAT(11,2) DEFAULT 0;
   DECLARE progreso FLOAT(11,2) DEFAULT 0;
@@ -79,7 +69,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `epics`
+-- Table structure for table `epics`
 --
 
 CREATE TABLE `epics` (
@@ -95,7 +85,7 @@ CREATE TABLE `epics` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `privilegios`
+-- Table structure for table `privilegios`
 --
 
 CREATE TABLE `privilegios` (
@@ -104,7 +94,7 @@ CREATE TABLE `privilegios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `privilegios`
+-- Dumping data for table `privilegios`
 --
 
 INSERT INTO `privilegios` (`id`, `nombre`) VALUES
@@ -114,7 +104,7 @@ INSERT INTO `privilegios` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `projects`
+-- Table structure for table `projects`
 --
 
 CREATE TABLE `projects` (
@@ -127,7 +117,18 @@ CREATE TABLE `projects` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reports`
+-- Table structure for table `project_epics`
+--
+
+CREATE TABLE `project_epics` (
+  `project_ID` int(11) NOT NULL,
+  `epic_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
 --
 
 CREATE TABLE `reports` (
@@ -139,7 +140,7 @@ CREATE TABLE `reports` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `roles`
+-- Table structure for table `roles`
 --
 
 CREATE TABLE `roles` (
@@ -149,7 +150,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `roles`
+-- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`id`, `nombre`, `descripcion`) VALUES
@@ -159,7 +160,7 @@ INSERT INTO `roles` (`id`, `nombre`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rol_privilegio`
+-- Table structure for table `rol_privilegio`
 --
 
 CREATE TABLE `rol_privilegio` (
@@ -168,7 +169,7 @@ CREATE TABLE `rol_privilegio` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `rol_privilegio`
+-- Dumping data for table `rol_privilegio`
 --
 
 INSERT INTO `rol_privilegio` (`idRol`, `idPrivilegio`) VALUES
@@ -179,7 +180,7 @@ INSERT INTO `rol_privilegio` (`idRol`, `idPrivilegio`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tickets`
+-- Table structure for table `tickets`
 --
 
 CREATE TABLE `tickets` (
@@ -202,7 +203,7 @@ CREATE TABLE `tickets` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -218,26 +219,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_ID`, `user_Name`, `user_Password`, `user_Phone`, `user_Mail`, `user_WeeklyAgilePoints`, `user_Skill`, `ticket_Assignee`, `ticket_Assignee_ID`) VALUES
-(1, 'Diego Vega', '$2a$12$FnpO1SU9uiu3MRPInTThIOg4qoTizzj1qaw3WqPEjj2tw1hQExLIe', '4426060404', 'diego@gmail.com', 0, '', NULL, NULL),
-(2, 'Arturo Cristián Díaz López', '$2a$12$FnpO1SU9uiu3MRPInTThIOg4qoTizzj1qaw3WqPEjj2tw1hQExLIe', '4421054338', 'arturo@outlook.com', 0, '', NULL, NULL),
-(3, 'Bernardo Gomez-Romero', '$2a$12$FnpO1SU9uiu3MRPInTThIOg4qoTizzj1qaw3WqPEjj2tw1hQExLIe', NULL, 'bernardo.gomez@dispatchhealth.com', 6, 'Front End', 'Bernardo Gomez-Romero', '61c9eb457c6f980070deda99'),
-(4, 'Giorgi Gelashvili', '$2a$12$1uyzx5zj9ZizDDly3WRqneEXjorXI8TblTRVd73iHpUcCbvKXTNs6', '4424987666', 'giorgi@dispatchhealth.com', 0, 'Front End', 'Giorgi Gelashvili', '62cdef6c6eba71983721e037'),
-(5, 'Alan Malagon', '$2a$12$tAIkkijN1BgoNnDY376XJesbe8fZwSFPKsEJY9ILORzA8ZbckabKG', '4424987666', 'alan@dispatchhealth', 0, 'Front End', 'Alan Malagon', '62cded1010fcc6f7ae3e5a73'),
-(6, 'Antonio Antillon', '1234', NULL, 'antonio@dispatchhealth.com', 0, 'Back End', 'Antonio Antillon', '61f81bb630f6b8006aa75942'),
-(7, 'Dan Cohn', '1234', NULL, 'dan@dispatchhealth.com', 0, 'Back End', 'Dan Cohn', '607f2fef739dd40069a4b2a3'),
-(8, 'Kevin Anderson', '1234', NULL, 'kevin@dispatchhealth.com', 0, 'Back End', 'Kevin Anderson', '62cdf0d01e326fd93012992d'),
-(9, 'Itzel Barreto', '1234', NULL, 'itzel@dispatchhealth.com', 0, 'Back End', 'Itzel Barreto', '62ce0b47a94a6f9c0efe90bf'),
-(10, 'Sam Lanker', '1234', '', 'sam@dispatchhealth.com', 0, 'Front End', 'Sam Lanker', '62cdf020afe495359d9d9b0b'),
-(11, 'Ashton Mitchell', '1234', NULL, 'ashton@dispatchhealth.com', 0, 'Front End', 'Ashton Mitchell', '61a3edabb0b630006afda9e4');
+(3, 'Bernardo Gomez-Romero', '$2a$12$FnpO1SU9uiu3MRPInTThIOg4qoTizzj1qaw3WqPEjj2tw1hQExLIe', NULL, 'bernardo.gomez@dispatchhealth.com', 6, 'Front End', 'Bernardo Gomez-Romero', '61c9eb457c6f980070deda99');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario_rol`
+-- Table structure for table `usuario_rol`
 --
 
 CREATE TABLE `usuario_rol` (
@@ -246,129 +237,134 @@ CREATE TABLE `usuario_rol` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `usuario_rol`
+-- Dumping data for table `usuario_rol`
 --
 
 INSERT INTO `usuario_rol` (`idUsuario`, `idRol`) VALUES
-(1, 2),
-(2, 2),
 (3, 1);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `epics`
+-- Indexes for table `epics`
 --
 ALTER TABLE `epics`
-  ADD PRIMARY KEY (`epic_ID`);
+  ADD PRIMARY KEY (`epic_ID`),
   ADD KEY `epic_Link` (`epic_Link`);
 
 --
--- Indices de la tabla `privilegios`
+-- Indexes for table `privilegios`
 --
 ALTER TABLE `privilegios`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `projects`
+-- Indexes for table `projects`
 --
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`project_ID`);
 
 --
--- Indices de la tabla `reports`
+-- Indexes for table `project_epics`
+--
+ALTER TABLE `project_epics`
+  ADD PRIMARY KEY (`project_ID`,`epic_ID`),
+  ADD KEY `epics` (`epic_ID`);
+
+--
+-- Indexes for table `reports`
 --
 ALTER TABLE `reports`
   ADD PRIMARY KEY (`report_ID`);
 
 --
--- Indices de la tabla `roles`
+-- Indexes for table `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `rol_privilegio`
+-- Indexes for table `rol_privilegio`
 --
 ALTER TABLE `rol_privilegio`
   ADD PRIMARY KEY (`idRol`,`idPrivilegio`),
   ADD KEY `idPrivilegio` (`idPrivilegio`);
 
 --
--- Indices de la tabla `tickets`
+-- Indexes for table `tickets`
 --
 ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`ticket_Id`);
+  ADD PRIMARY KEY (`ticket_Id`),
   ADD KEY `epic_Link` (`epic_Link`,`ticket_Assignee_ID`),
   ADD KEY `ticket_Assignee_ID` (`ticket_Assignee_ID`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_ID`);
+  ADD PRIMARY KEY (`user_ID`),
   ADD KEY `ticket_Assignee_ID` (`ticket_Assignee_ID`);
 
 --
--- Indices de la tabla `usuario_rol`
+-- Indexes for table `usuario_rol`
 --
 ALTER TABLE `usuario_rol`
   ADD KEY `usuario_rol_id_1` (`idUsuario`),
   ADD KEY `usuario_rol_id_2` (`idRol`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `epics`
+-- AUTO_INCREMENT for table `epics`
 --
 ALTER TABLE `epics`
   MODIFY `epic_ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `projects`
+-- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
   MODIFY `project_ID` int(100) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `reports`
+-- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
   MODIFY `report_ID` int(100) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `tickets`
+-- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
   MODIFY `ticket_Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=333;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `rol_privilegio`
+-- Constraints for table `project_epics`
+--
+ALTER TABLE `project_epics`
+  ADD CONSTRAINT `epics` FOREIGN KEY (`epic_ID`) REFERENCES `epics` (`epic_ID`),
+  ADD CONSTRAINT `projects` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`);
+
+--
+-- Constraints for table `rol_privilegio`
 --
 ALTER TABLE `rol_privilegio`
   ADD CONSTRAINT `rol_privilegio_id_1` FOREIGN KEY (`idRol`) REFERENCES `roles` (`id`),
   ADD CONSTRAINT `rol_privilegio_id_2` FOREIGN KEY (`idPrivilegio`) REFERENCES `privilegios` (`id`);
-
---
--- Filtros para la tabla `usuario_rol`
---
-ALTER TABLE `usuario_rol`
-  ADD CONSTRAINT `usuario_rol_id_1` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`user_ID`),
-  ADD CONSTRAINT `usuario_rol_id_2` FOREIGN KEY (`idRol`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
